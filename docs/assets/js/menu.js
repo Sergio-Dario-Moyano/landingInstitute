4421b82dc9cf6eadb3c4fcab99fc396f17ab8e0b
Sergio <sergiodariomoyano@gmail.com>
2023-04-27 21:07:43 -0300

    Se corrigieron algunos estilos y colores

--- docs/assets/js/menu.js
+++ docs/assets/js/menu.js
@@ -2,21 +2,21 @@ const lista = document.querySelector('#menu__responsive')
 const header = document.querySelector('#header__menu__responsive')
 const btnMenu = document.querySelector('#btn__menu')
 const btnClose = document.querySelector('#btn__close')
-const container = document.querySelector('#container')
+const body = document.querySelector('#body')
 
 lista.addEventListener('click', (e) => {
   if(e.target && e.target.tagName === 'A') {
     header.classList.remove('top')
     btnClose.style.display = "none"
     btnMenu.style.display = "block"
-    container.classList.remove('nonScroll')
+    body.classList.remove('nonScroll')
   }
 })
 
 btnMenu.addEventListener('click', () => {
   header.classList.remove('down')
   header.classList.add('top')
-  container.classList.add('nonScroll')
+  body.classList.add('nonScroll')
   btnMenu.style.display = "none"
   btnClose.style.display = "block"
 })
@@ -24,7 +24,7 @@ btnMenu.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
   header.classList.remove('top')
   header.classList.add('down')
-  container.classList.remove('nonScroll')
+  body.classList.remove('nonScroll')
   btnMenu.style.display = "block"
   btnClose.style.display = "none"
 })
